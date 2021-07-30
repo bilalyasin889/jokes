751ec48b70a7740317a4d62e48014b008ba51ae5
Bilal Yasin <byasin@tier2consulting.com>
2021-07-30 16:51:20 +0100

    JokeController Created. Closes #4.

---
+++ src/main/java/com/example/jokes/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.example.jokes.controllers;
+
+import com.example.jokes.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJokes(Model model){
+        model.addAttribute("joke", jokeService.getJokes());
+
+        return "index";
+    }
+}
